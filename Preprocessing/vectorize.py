af46a3723302d0fe49a7d39509972f33219c1103
Roshan Acharya <roshanacharya9808@gmail.com>
2025-11-22 23:07:59 +0545

    Save Vectorizer model

--- Preprocessing/vectorize.py
+++ Preprocessing/vectorize.py
@@ -1,6 +1,9 @@
 from sklearn.feature_extraction.text import CountVectorizer
-
+import pickle
 vectorizer = CountVectorizer()
 def vectorize_text(df):
     X = vectorizer.fit_transform(df['text'])
+
+    with open('./models/vectorizer.pkl', 'wb') as f:
+        pickle.dump(vectorizer, f)
     return X
